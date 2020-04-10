b213e60e23e93ccb2162d8d0b7599f609678b8c4
Agnieszka Letkiewicz <a.letkiewicz@gmail.com>
2020-04-10 23:17:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,8 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board,index)
-  if index.between?(1,9)
-  else position_taken?(board, index)
+  if index.between?(1,9) && position_taken?(board, index)
+    return true
+  else return false
   end
 end
